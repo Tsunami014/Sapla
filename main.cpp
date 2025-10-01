bc477c81f33380811a73322d1f563c9ef9e72c5b
Tsunami014 <tsunami014@duck.com>
2025-10-01 11:26:22 +1000

    Made the view a qgraphicsview

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <QApplication>
 #include <QLabel>
+#include <QWidget>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QVBoxLayout>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QLabel label("Hello!");
-    label.show();
+    QWidget window;
+
+    QGraphicsScene *scene = new QGraphicsScene();
+    scene->addRect(0, 0, 100, 100);
+
+    QGraphicsView *view = new QGraphicsView(scene);
+
+    QVBoxLayout *layout = new QVBoxLayout(&window);
+    layout->addWidget(view);
+    layout->setContentsMargins(0,0,0,0);
+
+    window.resize(800, 600);
+    window.show();
     return app.exec();
 }
 

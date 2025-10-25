3391be07a95b54a77a5ebca3f8e1dfe91182d15b
Tsunami014 <tsunami014@duck.com>
2025-10-25 13:41:41 +1100

    Updated the help screen

--- help.hpp
+++ help.hpp
@@ -1,5 +1,10 @@
 #pragma once
 #include <QString>
 
-const QString APP_HELP = "<h1>HELP</h1>";
+const QString APP_HELP = 
+"<h1>Sapla help</h1>"
+    "<h2>How the app works</h2>"
+        "<p>This app is a flashcard app where you complete flashcards in order to grow a tree.</p>"
+        "<p>You'll get stuff as you go along, but if not you can always check the 'This screen' in the 'Help' menu</p>"
+;
 

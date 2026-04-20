5d76de1b88a3c143709df1d0ab4ab306c3f80b3f
Tsunami014 <tsunami014@duck.com>
2026-04-20 15:52:08 +1000

    Added seeded randomness!

--- notes/features/hideShuffFeats.cpp
+++ notes/features/hideShuffFeats.cpp
@@ -1,6 +1,7 @@
 // Hidden and shuffle features
 #include "features.hpp"
 #include <QRandomGenerator>
+#include <QDateTime>
 
 const QRegularExpression scramblRe(R"((?<!\\)\.\.(.*?)\.\.)");
 QString ShuffledFeat::replacements(QString& txt, Side s) const {
@@ -86,6 +87,7 @@ const QRegularExpression hiddenRe(R"((?<!\\)\[\[((?:.|\n)*?)(?<!\\)\]\])");
 QString HiddenFeat::replacements(QString& txt, Side s) const {
     if (s == SIDE_NAME || s == SIDE_GETFC) return txt;
     if (s == SIDE_HIDE) return txt.remove(hiddenRe);
+    auto tse = QDateTime::currentMSecsSinceEpoch();
 
     auto it = hiddenRe.globalMatch(txt);
     int offs = 0;
@@ -93,11 +95,22 @@ QString HiddenFeat::replacements(QString& txt, Side s) const {
         auto m = it.next();
 
         QString conts = m.captured(1);
+        QString seed;
+        if (conts.contains("##")) {
+            int idx = conts.indexOf("##");
+            seed = conts.sliced(idx+2);
+            conts.slice(0, idx);
+        }
         QString repl;
         if (conts.contains("::") || conts.contains("//")) {
             QStringList opts = conts.split("//");
-            uint randomIndex = QRandomGenerator::global()->bounded(opts.size());
-            QString item = opts[randomIndex];
+            uint idx;
+            if (seed.isNull()) {
+                idx = QRandomGenerator::global()->bounded(opts.size());
+            } else {
+                idx = QRandomGenerator(tse+qHash(seed)).bounded(opts.size());
+            }
+            QString item = opts[idx];
 
             if (item.contains("::")) {
                 QStringList sides = item.split("::");
@@ -123,6 +136,7 @@ QString HiddenFeat::markup(QString& line) const {
         QString conts = m.captured(1);
         conts
             .replace("//", "<b style='color:" + col + "'>//</b>")
+            .replace("##", "<b style='color:" + col + "'>##</b>")
             .replace("::", "<b style='color:" + col + "'>::</b>")
         ;
         QString repl =
@@ -142,18 +156,17 @@ QMap<QString, QString> HiddenFeat::help() const {
            {"Always hidden text\n[[]]",
             "Always hides text (so can be seen as a 'comment')\n"
             "E.g. [[This text will NEVER be seen and doesn't affect anything!]]\n"
-            "Note features will still do stuff there though (e.g. if you had note templates or something it will still evaluate them)\n"
-            "Note templates will not work inside this."
+            "Note features will still do stuff there though (e.g. if you had note templates or something it will still evaluate them)"
         }, {"Hidden on sides\n[[::]]",
             "Only shows text if it's on a specific side\n"
             "E.g. `[[hi::bye]]` would show `hi` if it's on the front side of a flashcard and `bye` if it's on the back.\n"
-            "Leaving sides blank is ok (e.g. `[[front::]]`)\n"
-            "Note templates will not work inside this."
-        }, {"Randomly hidden\n[[//]]",
+            "Leaving sides blank is ok (e.g. `[[front::]]`)"
+        }, {"Randomly hidden\n[[//##]]",
             "Randomly selects one of the listed features to use\n"
             "E.g. `[[A//B]]` would show either A or B with a 50% chance\n"
             "This can be combined with hidden on sides, e.g. `[[choice1//choice2front::choice2back//choice3]]`)\n"
-            "Note templates will not work inside this."
+            "Using ## will set the seed so you can have the same result for different random choices (e.g. `[[1//2##a]] [[1//2##a]]`)\n"
+            "The seed can be any text or number input and MUST BE AT THE END (`[[1##seed//2]]` will use a seed of `seed//2`)."
         }
     };
 }
